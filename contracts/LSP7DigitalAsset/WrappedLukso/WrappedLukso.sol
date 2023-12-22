7e3c02a2cdfaf2923d9516b632c358f23aa1f544
heisenburgers <zkdaoorganization@gmail.com>
2023-12-22 06:38:10 +0400

    final version? idk

--- contracts/LSP7DigitalAsset/WrappedLukso/WrappedLukso.sol
+++ contracts/LSP7DigitalAsset/WrappedLukso/WrappedLukso.sol
@@ -12,45 +12,32 @@ contract WLYX7 is LSP7DigitalAsset, ILSP7Mintable {
     constructor(
         string memory name_,
         string memory symbol_,
+        address owner_,
         uint256 lsp4TokenType_,
         bool isNonDivisible_
     )
         LSP7DigitalAsset(
             name_,
             symbol_,
-            address(0),
+            owner_,
             lsp4TokenType_,
             isNonDivisible_
         )
     {}
 
-    modifier onlyThis() {
-        require(msg.sender == address(this), "Only the contract itself can call this.");
-        _;
-    }
-
-    function mint(
-        address to,
-        uint256 amount,
-        bool force,
-        bytes memory data
-    ) public virtual onlyThis {
-        _mint(to, amount, force, data);
-    }
-
     /// @dev Fallback, `msg.value` of LYX sent to this contract grants caller account a matching increase in WLYX7 token balance.
     receive() external payable override {
-        mint(msg.sender, msg.value, false, "");
+        _mint(msg.sender, msg.value, false, "");
     }
 
     /// @dev `msg.value` of LYX sent to this contract grants caller account a matching increase in WLYX7 token balance.
     function deposit() external payable {
-        mint(msg.sender, msg.value, false, "");
+        _mint(msg.sender, msg.value, false, "");
     }
 
     /// @dev `msg.value` of LYX sent to this contract grants `to` account a matching increase in WLYX7 token balance.
     function depositTo(address to) external payable {
-        mint(to, msg.value, false, "");
+        _mint(to, msg.value, false, "");
     }
 
     /// @dev Burn `value` WLYX7 token from caller account and withdraw matching LYX to the same.
@@ -59,7 +46,7 @@ contract WLYX7 is LSP7DigitalAsset, ILSP7Mintable {
     function withdraw(uint256 value) external {
         uint256 balance = balanceOf(msg.sender);
         require(balance >= value, "WLYX: burn amount exceeds balance");
-        _burn(msg.sender, balance - value, "");
+        _burn(msg.sender, value, "");
 
         // _transferLukso(msg.sender, value);
         (bool success, ) = msg.sender.call{value: value}("");
@@ -72,10 +59,17 @@ contract WLYX7 is LSP7DigitalAsset, ILSP7Mintable {
     function withdrawTo(address payable to, uint256 value) external {
         uint256 balance = balanceOf(msg.sender);
         require(balance >= value, "WLYX: burn amount exceeds balance");
-        _burn(msg.sender, balance - value, "");
+        _burn(msg.sender, value, "");
 
         // _transferLukso(to, value);
         (bool success, ) = to.call{value: value}("");
         require(success, "WLYX: LYX transfer failed");
     }
+
+    function mint(
+        address to,
+        uint256 amount,
+        bool force,
+        bytes memory data
+    ) external override {}
 }
